7cd7f5d5c1afde86798d305681944615478c74d0
Amar Basinac <abasin2@mail.walshcollege.edu>
2023-09-17 18:03:13 -0400

    First Additions

--- js/script.js
+++ js/script.js
@@ -1,131 +1,81 @@
-let winners = [];
-const choices = ["rock", "paper", "scissors"];
+let rockBtn = document.getElementById("rock");
+let paperBtn = document.getElementById("paper");
+let scissorsBtn = document.getElementById("scissors");
+let resultOutput = document.getElementById("result");
+let score = document.getElementById("score");
+let displayChoice = document.getElementById("display-choices");
+let playerChoice;
+let computerChoice;
 
-function resetGame() {
-    winners = [];
-    document.querySelector(".playerScore").textContent = "Score: 0";
-    document.querySelector(".computerScore").textContent = "Score: 0";
-    document.querySelector(".ties").textContent = "Ties: 0";
-    document.querySelector(".winner").textContent = "";
-    document.querySelector(".playerChoice").textContent = "";
-    document.querySelector(".computerChoice").textContent = "";
-    document.querySelector(".reset").style.display = "none";
+playerWins = 0;
+computerWins = 0;
+
+const getComputerChoice = () => {
+    const choices = ["rock", "paper", "scissors"];
+    const chosen = Math.floor(Math.random() * choices.length); 
+    return choices[chosen];
 }
 
-function startGame() {
-    //play the game until someone wins 5 times
-    let imgs = document.querySelectorAll("img");
-    imgs.forEach((img) =>
-      img.addEventListener("click", () => {
-        if (img.id) {
-          playRound(img.id);
-        }
-      })
-    );
-}
-
-function playRound(playerChoice) {
-    let wins = checkWins();
-    if (wins >= 5) {
-      return;
+const playRound = (playerChoice, computerChoice) => {
+    if (playerChoice === computerChoice) {
+        return "It's a Tie!";
     }
-
-    const computerChoice = computerSelect();
-
-    const winner = checkWinner(playerChoice, computerChoice);
-    winners.push(winner);
-    tallyWins();
-    displayRound(playerChoice, computerChoice, winner);
-    wins = checkWins();
-    if (wins == 5) {
-      //display end results
-      //change the button to visible,
-      //change the text to display winner
-      displayEnd();
-    }
-}
-
-function displayEnd() {
-    let playerWins = winners.filter((item) => item == "Player").length;
-
-    if (playerWins == 5) {
-      document.querySelector(".winner").textContent =
-        "You Won 5 Games, Congrats!";
-    } else {
-      document.querySelector(".winner").textContent =
-        "Sorry, the computer won 5 times";
-    }
-    document.querySelector(".reset").style.display = "flex";
-}
-
-function displayRound(playerChoice, computerChoice, winner) {
-    document.querySelector(".playerChoice").textContent = `You Chose: ${
-      playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)
-    }`;
-    document.querySelector(
-      ".computerChoice"
-    ).textContent = `The Computer Chose: ${
-      computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
-    }`;
-    displayRoundWinner(winner);
-}
-
-function displayRoundWinner(winner) {
-    if (winner == "Player") {
-      document.querySelector(".winner").textContent = "You won the Round!";
-    } else if (winner == "Computer") {
-      document.querySelector(".winner").textContent =
-        "The Computer won the Round";
-    } else {
-      document.querySelector(".winner").textContent = "The Round was a tie";
-    }
-}
-
-function tallyWins() {
-    const pWinCount = winners.filter((item) => item == "Player").length;
-    const cWinCount = winners.filter((item) => item == "Computer").length;
-    const ties = winners.filter((item) => item == "Tie").length;
-    document.querySelector(".playerScore").textContent = `Score: ${pWinCount}`;
-    document.querySelector(".computerScore").textContent = `Score: ${cWinCount}`;
-    document.querySelector(".ties").textContent = `Ties: ${ties}`;
-}
-
-function computerSelect() {
-    //todo - update the dom with the computer selection
-    const choice = choices[Math.floor(Math.random() * choices.length)];
-
-    document.querySelector(`.${choice}`).classList.add("active");
-
-    setTimeout(() => {
-      document.querySelector(`.${choice}`).classList.remove("active");
-    }, 700);
-
-    return choice;
-}
-
-function checkWins() {
-    const pWinCount = winners.filter((item) => item == "Player").length;
-    const cWinCount = winners.filter((item) => item == "Computer").length;
-    return Math.max(pWinCount, cWinCount);
-}
-
-function checkWinner(choice1, choice2) {
-    if (
-      (choice1 == "rock" && choice2 == "scissors") ||
-      (choice1 == "scissors" && choice2 == "paper") ||
-      (choice1 == "paper" && choice2 == "rock")
+    else if (
+        (playerChoice === "scissors" && computerChoice === "paper") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "rock" && computerChoice === "scissors")
     ) {
-      return "Player";
-    } else if (choice1 == choice2) {
-      return "Tie";
-    } else {
-      return "Computer";
+        return "Player Wins!";
+    }
+    else {
+        return "Computer Wins!";
     }
 }
 
-function setWins() {
-    const pWinCount = winners.filter((item) => item == "Player").length;
-    const cWinCount = winners.filter((item) => item == "Computer").length;
-    const ties = winners.filter((item) => item == "Tie").length;
+rockBtn.addEventListener("click", function() {
+    playerChoice = "rock";
+    let computerChoice = getComputerChoice();
+    result = playRound(playerChoice, computerChoice);
+    scoreCounter(result);
+});
+
+paperBtn.addEventListener("click", function() {
+    playerChoice = "paper";
+    let computerChoice = getComputerChoice();
+    result = playRound(playerChoice, computerChoice);
+    scoreCounter(result);
+});
+
+scissorsBtn.addEventListener("click", function() {
+    playerChoice = "scissors";
+    let computerChoice = getComputerChoice();
+    result = playRound(playerChoice, computerChoice);
+    scoreCounter(result);
+});
+
+function scoreCounter(result) {
+    if (result === "Player Wins!") {
+        resultOutput.textContent = result
+        playerWins++;
+    }
+    else if (result === "Computer Wins!") {
+        resultOutput.textContent = result
+        computerWins++;
+    }
+    displayScore(playerWins, computerWins)
 }
-startGame();
+
+function displayScore(playerWins, computerWins) {
+    score.textContent = `Player: ${playerWins} - Computer: ${computerWins}`
+
+    if (playerWins === 5) {
+        score.textContent = "Player wins the game!";
+    }
+    else if (computerWins === 5) {
+        score.textContent = "Computer wins the game!";
+    }
+}
+
+function displayChoices(playerChoice, computerChoice) {
+    displayChoice.textContent = `Player chose ${playerChoice} and Computer chose ${computerChoice}!`;
+}
